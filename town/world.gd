9df4b150e2628210d22a86bf3ae6f9931eabc3ad
Aunpyz <aunnop.kattiyanet@gmail.com>
2026-09-14 13:23:05 +0700

    Import assets, setup simple scene

--- town/world.gd
+++ town/world.gd
@@ -20,20 +20,9 @@ func _process(delta: float) -> void:
 		marker.new_job.connect(%Myphone.new_job)
 		marker.created()
 
-# FIXME: this is temporary
-var _times := 0
 func _random_house() -> Node3D:
 	#TODO: Maybe re-random if what is random turns into the same Node again
-	var target: Node3D
-	match _times:
-		0: 
-			target = $"../TownModel/Houses/House05/Anchor"
-		1:
-			target = $"../TownModel/Houses/House01/Anchor"
-		_:
-			target = _houses.pick_random()
-	_times = _times + 1
-	return target
+	return _houses.pick_random()
 
 func _on_delivered() -> void:
 	_delivering_house = null
